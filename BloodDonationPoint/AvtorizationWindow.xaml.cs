d0955c9dbecd2b7913752340caa02c6c7284204c
P1ck <P1ck@DESKTOP-J24GMEO>
2023-05-13 16:51:54 +0400

    Добавьте файлы проекта.

---
+++ BloodDonationPoint/AvtorizationWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BloodDonationPoint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AvtorizationWindow : Window
+    {
+        public AvtorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMinus_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnExit_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainWindowBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void tbPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (tbPassword.Password.Length > 0)
+            {
+                passWatermark.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                passWatermark.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win = new MainWindow();
+            win.Show();
+            this.Close();
+        }
+
+        private void btnExitWin_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
