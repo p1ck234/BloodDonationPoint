e801ebae39e2fc3ee4d992aaa9a35ba94750c6a7
Данил Ветров <danil-vetrov47@mail.ru>
2023-05-18 20:13:43 +0400

    Подключил базу данных. Прописал логику окна авторизации

--- BloodDonationPoint/AvtorizationWindow.xaml.cs
+++ BloodDonationPoint/AvtorizationWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BloodDonationPoint
 {
@@ -20,9 +24,11 @@ namespace BloodDonationPoint
     /// </summary>
     public partial class AvtorizationWindow : Window
     {
+        public static DataBase.BDBloodDonationEntities bd = new DataBase.BDBloodDonationEntities();
         public AvtorizationWindow()
         {
             InitializeComponent();
+            bd.Manager.Load();
         }
 
         private void btnMinus_MouseDown(object sender, MouseButtonEventArgs e)
@@ -55,16 +61,66 @@ namespace BloodDonationPoint
             }
         }
 
+        int counter = 0;
+        DispatcherTimer timer = new DispatcherTimer();
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow win = new MainWindow();
-            win.Show();
-            this.Close();
+            bd.Manager.Load();
+            try
+            {
+                if (bd.Manager.Local.Where(x => x.Login == tbLogin.Text && x.Password == tbPassword.Password).FirstOrDefault() != null)
+                {
+                    MainWindow win = new MainWindow();
+                    win.Show();
+                    this.Close();
+                }
+                else
+                {
+                    Exp("Пользователя с такими данными нет!");
+                    counter++;
+                }
+                if (counter >= 3)
+                {
+                    Inf("Вы ввели 3 раза неправильно пароль\nВвод пароля будет доступен через 10 секунд");
+                    counter = 0;
+                    timer = new DispatcherTimer();
+                    timer.Tick += new EventHandler(timer_Tick);
+                    timer.Interval = new TimeSpan(0, 0, 10);
+                    btnLogin.Visibility = Visibility.Hidden;
+                    timer.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Exp(ex.ToString());
+            }
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            btnLogin.Visibility = Visibility.Visible;
+        }
+        public static void Exp(string error)
+        {
+            MessageBox.Show(error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        public static void Inf(string inf)
+        {
+            MessageBox.Show(inf, "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         private void btnExitWin_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void tbLogin_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex input = new Regex(@"[a-zA-Z0-9]");
+            Match match = input.Match(e.Text);
+            if (!match.Success) 
+                { 
+                    e.Handled = true;   
+                }
         }
     }
 }
