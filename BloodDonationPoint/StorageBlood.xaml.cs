da74474f4a3c7f8ea329a15e35621241e63cb663
Данил Ветров <danil-vetrov47@mail.ru>
2023-05-27 19:59:29 +0400

    Описал логику кнопок хранища крови

--- BloodDonationPoint/StorageBlood.xaml.cs
+++ BloodDonationPoint/StorageBlood.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BloodDonationPoint.DataBase;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -30,12 +31,36 @@ namespace BloodDonationPoint
 
         private void btnDel_Click(object sender, RoutedEventArgs e)
         {
+            AvtorizationWindow.bd.BloodStorage.Load();
+            BloodStorage select = new BloodStorage();
+            if (dtgBloodStorage.SelectedItem != null)
+            {
+                select = (BloodStorage)dtgBloodStorage.SelectedItem;
+                if (MessageBox.Show("Вы действительно хотите удалить этот элемент из базы данных?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        AvtorizationWindow.bd.BloodStorage.Remove(select);
+                        AvtorizationWindow.bd.SaveChanges();
 
+                    }
+                    catch
+                    {
+                        AvtorizationWindow.Exp("Что-то пошло не так!");
+                    }
+                }
+            }
+            else
+            {
+                AvtorizationWindow.Exp("Вы ничего не выбрали!");
+            }
         }
 
         private void btnRel_Click(object sender, RoutedEventArgs e)
         {
-
+            AvtorizationWindow.bd.BloodStorage.Load();
+            dtgBloodStorage.ItemsSource = AvtorizationWindow.bd.BloodStorage.Local.OrderBy(x => x.ID);
+            AvtorizationWindow.Inf("Страница была обновлена");
         }
     }
 }
